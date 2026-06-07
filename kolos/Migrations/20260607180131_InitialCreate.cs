e29e7597828383fa7e4ab794cf248ba3a20591e3
Adam Gyurjyan <s31193@pjwstk.edu.pl>
2026-06-07 20:01:58 +0200

    feat: add db migrations

---
+++ kolos/Migrations/20260607180131_InitialCreate.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace kolos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artist",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artist", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gallery",
+                columns: table => new
+                {
+                    GalleryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EstablishedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gallery", x => x.GalleryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Artwork",
+                columns: table => new
+                {
+                    ArtworkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    YearCreated = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artwork", x => x.ArtworkId);
+                    table.ForeignKey(
+                        name: "FK_Artwork_Artist_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artist",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exhibition",
+                columns: table => new
+                {
+                    ExhibitionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GalleryId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NumberOfArtworks = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exhibition", x => x.ExhibitionId);
+                    table.ForeignKey(
+                        name: "FK_Exhibition_Gallery_GalleryId",
+                        column: x => x.GalleryId,
+                        principalTable: "Gallery",
+                        principalColumn: "GalleryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExhibitionArtwork",
+                columns: table => new
+                {
+                    ExhibitionId = table.Column<int>(type: "int", nullable: false),
+                    ArtworkId = table.Column<int>(type: "int", nullable: false),
+                    InsuranceValue = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExhibitionArtwork", x => new { x.ExhibitionId, x.ArtworkId });
+                    table.ForeignKey(
+                        name: "FK_ExhibitionArtwork_Artwork_ArtworkId",
+                        column: x => x.ArtworkId,
+                        principalTable: "Artwork",
+                        principalColumn: "ArtworkId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExhibitionArtwork_Exhibition_ExhibitionId",
+                        column: x => x.ExhibitionId,
+                        principalTable: "Exhibition",
+                        principalColumn: "ExhibitionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artist",
+                columns: new[] { "ArtistId", "BirthDate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "John", "Doe" },
+                    { 2, new DateTime(2003, 2, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Marian", "Gostek" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Gallery",
+                columns: new[] { "GalleryId", "EstablishedDate", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2023, 2, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Gallery 1" },
+                    { 2, new DateTime(2024, 7, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Gallery 2" },
+                    { 3, new DateTime(2021, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Gallery 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artwork",
+                columns: new[] { "ArtworkId", "ArtistId", "Title", "YearCreated" },
+                values: new object[,]
+                {
+                    { 1, 1, "Artwork 1", 2025 },
+                    { 2, 2, "Artwork 2", 2003 },
+                    { 3, 1, "Artwork 3", 1992 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Exhibition",
+                columns: new[] { "ExhibitionId", "EndDate", "GalleryId", "NumberOfArtworks", "StartDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2003, 2, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 2, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Exhibition 1" },
+                    { 2, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 5, new DateTime(2025, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Exhibition 2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ExhibitionArtwork",
+                columns: new[] { "ArtworkId", "ExhibitionId", "InsuranceValue" },
+                values: new object[,]
+                {
+                    { 1, 1, 1999.5m },
+                    { 3, 2, 9000.23m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artwork_ArtistId",
+                table: "Artwork",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exhibition_GalleryId",
+                table: "Exhibition",
+                column: "GalleryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExhibitionArtwork_ArtworkId",
+                table: "ExhibitionArtwork",
+                column: "ArtworkId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExhibitionArtwork");
+
+            migrationBuilder.DropTable(
+                name: "Artwork");
+
+            migrationBuilder.DropTable(
+                name: "Exhibition");
+
+            migrationBuilder.DropTable(
+                name: "Artist");
+
+            migrationBuilder.DropTable(
+                name: "Gallery");
+        }
+    }
+}
